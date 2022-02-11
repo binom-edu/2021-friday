9e8129314e6d0f4b595aa6c60afde33689b76392
Alexander Klindyuk <info@binom-r.ru>
2022-02-11 18:03:20 +0300

    Занятие 2. Быки и коровы. Финиш

--- mastermind/main.py
+++ mastermind/main.py
@@ -13,5 +13,39 @@ def generateSecret():
     # return ans
     return ''.join(digits[:NUM_DIGITS])
 
-# проверяем работу функции
-print(generateSecret())
+# проверка ответа: подсчет быков и коров
+def check(guess: str, secret: str) -> dict:
+    ans = {'bulls': 0, 'cows': 0}
+    for i in range(NUM_DIGITS):
+        if guess[i] == secret[i]:
+            ans['bulls'] += 1
+        elif guess[i] in secret:
+            ans['cows'] += 1
+    return ans
+
+# получение ответа пользователя и проверка корректности
+def getUserMove() -> str:
+    while True:
+        guess = input('Введите ваше число: ')
+        if len(guess) != NUM_DIGITS:
+            print('Длина ответа не совпадает с загаданным числом')
+            continue
+        if not guess.isdigit():
+            print('Ответ содержит недопустимые символы')
+            continue
+        return guess
+
+# основная программа
+print('Быки и коровы')
+secret = generateSecret()
+print(f'Загадано число из {NUM_DIGITS} цифр. У вас есть {MAX_GUESS} попыток, чтобы его отгадать.')
+for i in range(MAX_GUESS):
+    print('Попытка', i + 1)
+    guess = getUserMove()
+    result = check(guess, secret)
+    print('Быки:', result['bulls'], 'Коровы:', result['cows'])
+    if guess == secret:
+        print('Вы выиграли!')
+        break
+else:
+    print('Попытки закончились. Было загадано число', secret)
