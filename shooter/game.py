83c5633cacefd7c7f1e6da20c9548af84159de90
binom-edu <info@binom-r.ru>
2022-12-24 11:24:51 +0300

    Занятие 10

--- shooter/game.py
+++ shooter/game.py
@@ -1,4 +1,24 @@
+# Space Shooter (Redux, plus fonts and sounds) by Kenney Vleugels (www.kenney.nl)
+
 import pygame, os, random
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = player_img
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 20
+    def update(self):
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.rect.x -= 5
+        if keystate[pygame.K_RIGHT]:
+            self.rect.x += 5
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
 
 WIDTH = 400
 HEIGHT = 600
@@ -11,6 +31,11 @@ pygame.display.set_caption('Аркадный шутер')
 clock = pygame.time.Clock()
 img_dir = os.path.join(os.path.dirname(__file__), 'img')
 snd_dir = os.path.join(os.path.dirname(__file__), 'snd')
+player_img = pygame.image.load(os.path.join(img_dir, 'playerShip.png'))
+
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
 
 game_on = True
 while game_on:
@@ -20,7 +45,9 @@ while game_on:
         if event.type == pygame.QUIT:
             game_on = False
     # обновление
-
+    all_sprites.update()
     # рендеринг
-
+    screen.fill((0, 0, 0))
+    all_sprites.draw(screen)
+    pygame.display.flip()
 pygame.quit()
